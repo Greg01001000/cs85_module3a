5ffd87709524da0831ffc73883ccf62bd69a8806
Greg H <grarha@gmail.com>
2026-07-02 13:51:04 -0700

    Boilerplate HTML & PHP, passed smoke test

---
+++ ContactForm.php
@@ -0,0 +1,13 @@
+<!--CS 85 Module 3, Assignment 3A, by Gregory Hagen, 7/2/26-->
+<!DOCTYPE HTML>
+<html lang='en'>
+    <head>
+        <meta charset='UTF-8'>
+        <title>Contact Me</title>
+    </head>
+<body>
+    <?php
+
+    ?>
+</body>
+</html>
