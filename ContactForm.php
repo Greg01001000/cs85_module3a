0fa569a050212836dc8cdbe878669a9332cafcd3
Greg H <grarha@gmail.com>
2026-07-02 14:35:25 -0700

    Added function to validate input, smoke tested

--- ContactForm.php
+++ ContactForm.php
@@ -7,7 +7,22 @@
     </head>
 <body>
     <?php
+        /* If user-supplied field is empty, notify that it's required and keep
+        count of the error. If not empty, remove slashes and trailing & leading
+        spaces. Return cleaned field data. */
+        function validateInput($data, $fieldName) {
+            global $errorCount;
+            if (empty($data)) {
+                echo "\"$fieldName\" is a required field.<br />\n";
+                ++$errorCount;
+                $retval = "";
+            }
+            else {      // Only clean up the input if it isn't empty
+                $retval = trim($data);
+                $retval = stripslashes($retval);
+            }
 
+            return($retval); }
     ?>
 </body>
 </html>
